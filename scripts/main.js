51a87a9dcbaaa0a87db318b4b01dae4b5716a69d
n0obDeveloper <71236828+n0obDeveloper@users.noreply.github.com>
2020-09-14 06:07:01 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+
+/* MDN DOC */
+
+//for shifting between two images on clicking
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute('src','images/firefox-icon.png');
+    }
+}   
+
+//taking input from the user with the help of a button then storing the same into a local storage... 
+// ...and retreiving it to display on the heading(h1) of the webpage
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+
+  function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }
